91d8f1b8853e54ffd05eacefcc10446b5677c4a7
Henrik Wolf <h.wolf@amolf.nl>
2026-07-10 17:43:47 +0200

    fix tests for new edgeplot signature

--- test/runtests.jl
+++ test/runtests.jl
@@ -279,8 +279,8 @@ end
 end
 
 @testset "test empty edge plot" begin
-    GraphMakie.edgeplot(GraphMakie.Line{Point{2, Float32}}[])
-    GraphMakie.edgeplot(GraphMakie.AbstractPath{Point{2, Float32}}[])
+    GraphMakie.edgeplot(GraphMakie.Line{Point{2, Float32}}[], ())
+    GraphMakie.edgeplot(GraphMakie.AbstractPath{Point{2, Float32}}[], ())
 
     # test empty edge color
     graphplot(SimpleGraph(2); edge_color=Symbol[])
